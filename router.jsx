65f658f30fabe390ea94bf4af3978852d4f0a73e
Benja-Dev <begalan@outlook.es>
2025-02-13 16:52:02 -0600

    Actualizando rutas

--- router.jsx
+++ router.jsx
@@ -1,12 +1,64 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Layout from './src/layouts/Layout';
+import { createBrowserRouter } from "react-router-dom"
+import DashboardLayout from "./src/layouts/DashboardLayout"
+import WelcomeLayout from "./src/layouts/WelcomeLayout"
+import AuthLayout from "./src/layouts/AuthLayout"
 
+import Login from "./src/views/Login"
+import Register from "./src/views/Register"
+import Welcome from "./src/views/Welcome"
+import Events from "./src/views/Events"
+import GettingStarted from "./src/views/GettingStarted"
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />
-    }
-])  
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <GettingStarted />,
+      },
+      {
+        path: "getting-started",
+        element: <GettingStarted />,
+      },
+    ],
+  },
+  {
+    path: "/welcome",
+    element: <WelcomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Welcome />,
+      },
+    ],
+  },
+  {
+    path: "/manage",
+    element: <WelcomeLayout />,
+    children: [
+      {
+        path: "events",
+        element: <Events />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+])
 
-export default router;
+export default router
+
