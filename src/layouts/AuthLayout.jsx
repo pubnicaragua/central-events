2d8f33ae422b3dc0e468bd919861a31de774db2f
Benja-Dev <begalan@outlook.es>
2025-02-14 08:22:04 -0600

    Simplificacion de la ruta del logo

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
       <main className="max-w-4xl w-full bg-white rounded-lg shadow-md overflow-hidden">
         <div className="flex flex-col items-center">
           <div className="md:w-1/2 p-6 flex items-center justify-center">
-            <img src="../../public/logo.jpg" alt="Logo de Central Events" className="w-40 h-auto" />
+            <img src="/logo.jpg" alt="Logo de Central Events" className="w-40 h-auto" />
           </div>
           <div className="md:w-1/2">
             <Outlet />
