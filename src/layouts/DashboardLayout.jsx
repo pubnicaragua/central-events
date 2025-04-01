2a75720072f193a84ef5163c89c501bdaff4c4c7
Benja-Dev <begalan@outlook.es>
2025-04-01 09:55:30 -0600

    Corrigiendo importacion

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
 //IMPORTANDO SIDEBAR
-import { Sidebar } from "../components/Sidebar"
+import  Sidebar  from "../components/Sidebar"
 
 export default function DashboardLayout() {
   return (
