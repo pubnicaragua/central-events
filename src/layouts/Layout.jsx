a7602881ce78a5d1e5a8dc89a72d05a7a8801ffb
Benja-Dev <begalan@outlook.es>
2025-04-12 11:27:23 -0600

    Nuevo layout para uso de check in en dispositivos moviles

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom"
+import { EmployeeMobileMenu } from "../components/EmployeeMobileMenu"
 
-export default function Layout(){
-    return (
-        <div className="font-bold">
-            Layout
-
-            <Outlet />
-        </div>
-    )
+function DashboardLayout () {
+  return (
+    <div className="flex min-h-screen">
+      <EmployeeMobileMenu />
+      <div className="flex-1">
+        <main className="flex-1 p-4 md:p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  )
 }
+
+export default DashboardLayout
